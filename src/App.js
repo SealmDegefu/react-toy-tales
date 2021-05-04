253a49bbd6d44177cd3880d2cd037f3047d67909
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-05-04 12:52:24 -0700

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,42 +1,112 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Header from './components/Header'
-import ToyForm from './components/ToyForm'
-import ToyContainer from './components/ToyContainer'
+import Header from "./components/Header";
+import ToyForm from "./components/ToyForm";
+import ToyContainer from "./components/ToyContainer";
 
-
-class App extends React.Component{
-
+class App extends React.Component {
   state = {
-    display: false
+    toys: [],
+    display: false,
+  };
+  componentDidMount() {
+    fetch(" http://localhost:3000/toys")
+      .then((res) => res.json())
+      .then((toysData) =>
+        this.setState({
+          toys: toysData,
+        })
+      );
   }
+
+  addToy = (newToy) => {
+    console.log(newToy);
+    let postOPtion = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+      },
+      body: JSON.stringify(newToy),
+    };
+
+    fetch(" http://localhost:3000/toys", postOPtion)
+      .then((res) => res.json())
+      .then((addedToy) =>
+        this.setState({ toys: [...this.state.toys, addedToy] })
+      );
+  };
+
+  like = (newLike) => {
+    
+    let patchOPtion = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+      },
+      body: JSON.stringify(newLike),
+    };
+
+    fetch(" http://localhost:3000/toys/" + newLike.id, patchOPtion)
+     
+  };
+
+  handleLikeClick = (event) =>{
+    let likeIndex = event.parentNode.id - 1
+   let newLikes = [...this.state.toys]
+
+   let toyLikes = (newLikes[likeIndex])
+   ++toyLikes.likes
+
+this.like(toyLikes)
+
+   newLikes = toyLikes
+    console.log(newLikes)
+    
+    this.setState(newLikes = toyLikes)
+  }
+
+  deleteToy = (id) => {
+    fetch(" http://localhost:3000/toys/" + id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((newToy) =>
+        this.setState({
+          toys: this.state.toys.filter((toy) => toy.id !== id),
+        })
+      );
+  };
 
   handleClick = () => {
-    let newBoolean = !this.state.display
+    let newBoolean = !this.state.display;
     this.setState({
-      display: newBoolean
-    })
-  }
+      display: newBoolean,
+    });
+  };
 
-  render(){
+  render() {
     return (
       <>
-        <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
-          null
-        }
+        <Header />
+        {this.state.display ? <ToyForm addToy={this.addToy} /> : null}
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer
+          toys={this.state.toys}
+          delete={this.deleteToy}
+          handleLikeClick={this.handleLikeClick}
+        />
       </>
     );
   }
-
 }
 
 export default App;
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,22 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ToyCard from "./ToyCard";
 
 class ToyForm extends Component {
+  state = {
+    name: "",
+    image: "",
+    likes: 0
+  };
+  newValue = (event) => {
+    event.preventDefault();
+    this.props.addToy(this.state)
+  };
+
+  handleName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleImage = (event) => {
+    this.setState({
+      image: event.target.value,
+    });
+  };
 
   render() {
     return (
-      <div className="container">
-        <form className="add-toy-form">
+      <div className="container" >
+        <form className="add-toy-form" onSubmit={this.newValue}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
-          <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
-          <br/>
-          <input type="submit" name="submit" value="Create New Toy" className="submit"/>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter a toy's name..."
+            className="input-text"
+            value={this.state.name}
+            onChange={this.handleName}
+          />
+          <br />
+          <input
+            type="text"
+            name="image"
+            placeholder="Enter a toy's image URL..."
+            className="input-text"
+            value={this.state.image}
+            onChange={this.handleImage}
+          />
+          <br />
+          <input
+            type="submit"
+            name="submit"
+            value="Create New Toy"
+            className="submit"
+          />
         </form>
       </div>
     );
   }
-
 }
 
 export default ToyForm;
